6f956f4b235bfb91dc2befbb63245f0b620dbefa
heyongqiang <1498833800@qq.com>
2022-06-09 09:56:30 +0800

    修改tailwind配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /*
  * @Author: your name
  * @Date: 2022-05-04 19:20:00
- * @LastEditTime: 2022-05-04 19:21:26
- * @LastEditors: your name
+ * @LastEditTime: 2022-06-09 09:55:57
+ * @LastEditors: heyongqiang 1498833800@qq.com
  * @Description: 样式控件配置
  * @FilePath: /react-demo/tailwind.config.js
  */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
   },
