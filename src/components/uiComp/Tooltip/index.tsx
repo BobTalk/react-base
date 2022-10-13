19cf52bdb99acec2d8c4389bee88033358f053c9
heyongqiang <1498833800@qq.com>
2022-10-13 14:33:12 +0800

    end添加递归模式的tooltip

--- src/components/uiComp/Tooltip/index.tsx
+++ src/components/uiComp/Tooltip/index.tsx
@@ -2,16 +2,17 @@
  * @Author: heyongqiang 1498833800@qq.com
  * @Date: 2022-10-12 14:44:42
  * @LastEditors: heyongqiang 1498833800@qq.com
- * @LastEditTime: 2022-10-13 14:18:20
+ * @LastEditTime: 2022-10-13 14:31:48
  * @FilePath: /react-base/src/components/uiComp/Tooltip/index.tsx
  * @Description: tooltip组件
  */
 import { Button, Tooltip } from "antd";
-import { memo, useState } from "react";
+import { memo, useRef, useState, useEffect } from "react";
 import RecursionComp from "./child";
 const TooltipComp = (props) => {
   const [tooltipArr, setTooltipArr] = useState(props.data);
   const [_, setRefresh] = useState({});
+  const TooltipBoxRef = useRef();
   const tooltipChangeCb = (open, currentItem) => {
     treeForeach((item) => {
       if (item.id == currentItem.id) {
@@ -29,6 +30,18 @@ const TooltipComp = (props) => {
     setRefresh({});
   };
 
+  useEffect(() => {
+    const exc = function (e) {
+      if (!TooltipBoxRef.current.contains(e.target)) {
+        nodeClickCb({});
+      }
+    };
+    window.addEventListener("click", exc);
+    return () => {
+      window.removeEventListener("click", exc);
+    };
+  }, []);
+
   const treeForeach = (fn, dataSource) => {
     dataSource.forEach((item) => {
       item.children && treeForeach(fn, item.children);
@@ -36,7 +49,7 @@ const TooltipComp = (props) => {
     });
   };
   return (
-    <div>
+    <div ref={TooltipBoxRef}>
       <RecursionComp
         nodeClick={nodeClickCb}
         onOpenChange={tooltipChangeCb}
@@ -48,4 +61,7 @@ const TooltipComp = (props) => {
 TooltipComp.defaultProps = {
   data: [],
 };
-export default TooltipComp;
+export default memo(TooltipComp, () => {
+  console.log("sdsds");
+  return false;
+});
