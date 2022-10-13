52ffd7978fc1b1a69ba3c55df50b7343ac01857d
heyongqiang <1498833800@qq.com>
2022-10-13 13:44:05 +0800

    end添加递归模式的tooltip

--- src/components/uiComp/Tooltip/index.tsx
+++ src/components/uiComp/Tooltip/index.tsx
@@ -2,7 +2,7 @@
  * @Author: heyongqiang 1498833800@qq.com
  * @Date: 2022-10-12 14:44:42
  * @LastEditors: heyongqiang 1498833800@qq.com
- * @LastEditTime: 2022-10-13 11:16:57
+ * @LastEditTime: 2022-10-13 11:25:08
  * @FilePath: /react-base/src/components/uiComp/Tooltip/index.tsx
  * @Description: tooltip组件
  */
@@ -21,10 +21,13 @@ const TooltipComp = (props) => {
     setRefresh({});
   };
   const nodeClickCb = (currentNode) => {
-    treeForeach((item) => {
-      item.visible = false;
-    }, tooltipArr);
-    setRefresh({});
+    console.log("currentNode: ", currentNode);
+    let currentDataRoot = findParents(tooltipArr, currentNode.parentId);
+    console.log("currentDataRoot: ", currentDataRoot);
+    // treeForeach((item) => {
+    //   item.visible = false;
+    // }, tooltipArr);
+    // setRefresh({});
   };
 
   const treeForeach = (fn, dataSource) => {
@@ -33,6 +36,31 @@ const TooltipComp = (props) => {
       fn(item);
     });
   };
+  // 根据当前数据Id查找父级
+  function findParents(array, id, children = "children") {
+    let parentArray = [];
+    if (array.length === 0) {
+      return parentArray;
+    }
+    function recursion(arrayNew, id, children) {
+      for (let i = 0; i < arrayNew.length; i++) {
+        let node = arrayNew[i];
+        if (node.id === id) {
+          parentArray.unshift(node);
+          recursion(array, node.parentId, children);
+          break;
+        } else {
+          let childList = node[children];
+          if (!!childList) {
+            recursion(childList, id, children);
+          }
+        }
+      }
+      return parentArray;
+    }
+    parentArray = recursion(array, id, children);
+    return parentArray;
+  }
   return (
     <RecursionComp
       nodeClick={nodeClickCb}
--- src/views/Bar/index.tsx
+++ src/views/Bar/index.tsx
@@ -6,20 +6,56 @@ const BarComp = (props) => {
     {
       label: "高级计算",
       id: "2",
+      parentId: null,
+      visible: false,
+      children: [
+        {
+          label: "求和",
+          id: "2-1",
+          parentId: "2",
+          visible: false,
+          children: [
+            {
+              label: "数值",
+              id: "2-1-1",
+              parentId: "2-1",
+              visible: false,
+            },
+          ],
+        },
+        {
+          label: "最大值",
+          id: "2-2",
+          parentId: "2",
+        },
+        {
+          label: "最小值",
+          id: "2-3",
+          parentId: "2",
+        },
+        {
+          label: "平均值",
+          id: "2-4",
+          parentId: "2",
+        },
+      ],
     },
     {
       label: "聚合方式",
       id: "1",
+      parentId: null,
       visible: false,
       children: [
         {
           label: "求和",
           id: "1-1",
+          parentId: "1",
           visible: false,
           children: [
             {
               label: "数值",
               id: "1-1-1",
+              parentId: "1-1",
               visible: false,
             },
           ],
@@ -27,14 +63,17 @@ const BarComp = (props) => {
         {
           label: "最大值",
           id: "1-2",
+          parentId: "1",
         },
         {
           label: "最小值",
           id: "1-3",
+          parentId: "1",
         },
         {
           label: "平均值",
           id: "1-4",
+          parentId: "1",
         },
       ],
     },
