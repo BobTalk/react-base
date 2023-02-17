05f7b40a7fff72617b2a39ddd5541e6ccba31359
heyongqiang <1498833800@qq.com>
2023-02-17 18:33:22 +0800

    处理错误捕获组建

--- src/components/catchError/index.tsx
+++ src/components/catchError/index.tsx
@@ -2,38 +2,42 @@
  * @Author: heyongqiang 1498833800@qq.com
  * @Date: 2022-11-07 10:11:10
  * @LastEditors: heyongqiang 1498833800@qq.com
- * @LastEditTime: 2022-11-07 13:53:04
+ * @LastEditTime: 2023-02-17 18:33:01
  * @FilePath: /react-base/src/components/catchError/index.tsx
  * @Description: 错误捕获
  */
 import React from "react";
-import { Button } from 'antd'
-import { NavLink } from "react-router-dom";
 export default class ErrorBoundaryPage extends React.Component {
-    state = {
-        hasError: false,
-        error: null,
-    }
-
-    static getDerivedStateFromError(error) {
-        return {
-            hasError: true,
-            error: error.toString()
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasError: false,
         }
     }
-    goBack() {
-        window.open('/', '_self')
+
+
+    // static getDerivedStateFromError(error) {
+    //     return {
+    //         hasError: true,
+    //         error: error.toString()
+    //     }
+    // }
+    componentDidCatch(error, info) {
+        let errorUrl = info.componentStack
+        let Reg = /(?<=\()(.+?)(?=\))/g
+        let splitRes = errorUrl.match(Reg)
+        this.setState({
+            hasError: true,
+            error: error.toString(),
+            errorSite: splitRes[0]
+        })
     }
     render() {
         if (this.state.hasError) {
             return (<>
                 <span>{this?.props?.fallback}</span>
                 <span>{this.state.error}</span>
-                <Button type="primary" danger onClick={() => {
-                    this.goBack()
-                }}>
-                    返回
-                </Button>
+                <p>{this.state.errorSite}</p>
             </>)
         }
         return this.props.children
