6aa02349beea96e71d782623594441518a436745
heyongqiang <1498833800@qq.com>
2023-02-20 14:57:26 +0800

    修改useLayoutEffect

--- src/views/layoutEffect/index.tsx
+++ src/views/layoutEffect/index.tsx
@@ -1,18 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2022-05-06 14:23:43
+ * @LastEditTime: 2023-02-20 14:55:37
+ * @LastEditors: heyongqiang 1498833800@qq.com
+ * @Description: useLayoutEffect 在挂载真实DOM元素之前使用
+ * @FilePath: /react-demo/src/views/layoutEffect/index.tsx
+ */
 import {
   forwardRef,
+  Fragment,
   useImperativeHandle,
   useLayoutEffect,
   useState,
 } from "react";
-
-/*
- * @Author: your name
- * @Date: 2022-05-06 14:23:43
- * @LastEditTime: 2022-05-06 14:49:30
- * @LastEditors: Please set LastEditors
- * @Description: useLayoutEffect 在挂载真实DOM元素之前使用
- * @FilePath: /react-demo/src/views/layoutEffect/index.tsx
- */
 const LayoutEffectComp = forwardRef((props, ref) => {
   useImperativeHandle(ref, () => ({}));
   const [width, setWidth] = useState(0);
@@ -25,6 +25,6 @@ const LayoutEffectComp = forwardRef((props, ref) => {
     window.addEventListener("mousemove", resize);
     return () => window.removeEventListener("mousemove", resize);
   }, []);
-  return [width,<br/>, height];
+  return [width, <br />, height];
 });
 export default LayoutEffectComp;
