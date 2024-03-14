5be612be44c16321dc5709af61d7d3d9bad5a47d
heyongqiang <1498833800@qq.com>
2024-03-14 22:45:51 +0800

    添加 tabs 组件

--- src/views/Tabs/index.tsx
+++ src/views/Tabs/index.tsx
@@ -1,14 +1,22 @@
+/*
+ * @Author: heyongqiang 1498833800@qq.com
+ * @Date: 2024-03-14 22:28:25
+ * @LastEditors: heyongqiang 1498833800@qq.com
+ * @LastEditTime: 2024-03-14 22:45:15
+ * @FilePath: /react-base/src/views/Tabs/index.tsx
+ * @Description: tabs
+ */
 import { useState } from "react";
-import { Tabs, Tab } from "react-tabs-scrollable";
+import { Tabs } from "react-tabs-scrollable";
 import "react-tabs-scrollable/dist/rts.css";
 
 function TabsComp() {
-    let [activeTab] = useState(1)
-    return <Tabs activeTab={activeTab}>
+  let [activeTab] = useState(1)
+  return <Tabs activeTab={activeTab}>
     {[...Array(10).keys()].map((item) => (
-      <Tab key={item}>Tab {item}</Tab>
+      <p key={item}>Tab {item}</p>
     ))}
   </Tabs>
- }
+}
 
 export default TabsComp
